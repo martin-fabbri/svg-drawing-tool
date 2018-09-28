b23f1a4f3b8e2d4bde4fc152465c038d22cf7fac
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-09-27 23:12:39 -0700

    Gating Area

---
+++ src/graph-panel/components/gate-layer/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+interface IProps {
+    debug?: boolean;
+}
+
+interface IState {
+    isGatingActive: boolean;
+    gates:
+}
+
+class GateLayer extends React.Component {
+    constructor() {
+
+    }
+}
+
+export default GateLayer;
