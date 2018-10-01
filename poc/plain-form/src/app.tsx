dc802c24ca8837ab94c141c552a13d3f72ca7d6d
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-10-01 01:33:00 -0700

    Rect Drawing.

--- poc/plain-form/src/app.tsx
+++ poc/plain-form/src/app.tsx
@@ -1,10 +1,41 @@
+import {colors, createMuiTheme, CssBaseline, MuiThemeProvider, Paper} from "@material-ui/core";
 import * as React from 'react';
 import Form from './form';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            dark: colors.purple[700],
+            light: colors.purple[300],
+            main: colors.purple[500],
+        },
+        secondary: {
+            dark: colors.green[700],
+            light: colors.green[300],
+            main: colors.green[500],
+        },
+    },
+});
+
+// const styles = theme => ({
+//     icon: {
+//         marginRight: theme.spacing.unit,
+//     },
+//     root: {
+//         paddingTop: theme.spacing.unit * 20,
+//         textAlign: 'center',
+//     },
+// });
 
 class App extends React.Component {
   public render() {
     return (
-      <Form />
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <Paper>
+                <Form />
+            </Paper>
+        </MuiThemeProvider>
     );
   }
 }
--- poc/plain-form/src/form.tsx
+++ poc/plain-form/src/form.tsx
@@ -1,11 +1,14 @@
+import {Grid, TextField} from "@material-ui/core";
+import Button from '@material-ui/core/Button';
 import * as React from 'react';
+import { ChangeEvent, FormEvent } from 'react';
 
 interface IProps {
     debug?: boolean;
 }
 
 interface IState {
-    firstname: string;
+    firstName: string;
     lastName: string;
     username: string;
     email: string;
@@ -17,7 +20,7 @@ class Form extends React.Component<IProps, IState> {
         super(props);
         this.state = {
             email: '',
-            firstname: '',
+            firstName: '',
             lastName: '',
             password: '',
             username: '',
@@ -25,17 +28,88 @@ class Form extends React.Component<IProps, IState> {
     }
 
     public render() {
-        const {email, firstname, lastName, password, username} = this.state;
+        const {email, firstName, lastName, password, username} = this.state;
         return (
-            <form action="">
-                <input type="text" placeholder='First Name' value={firstname}/>
-                <input type="text" placeholder='Last Name' value={lastName}/>
-                <input type="text" placeholder='Email' value={email}/>
-                <input type="text" placeholder='Username' value={username}/>
-                <input type="password" placeholder='Password' value={password}/>
+            <form onSubmit={this.handleSubmit}>
+                <Grid container={true} spacing={24}>
+                    <Grid item={true} xs={12}>
+                        <TextField
+                            name="firstName"
+                            label="First Name"
+                            margin="dense"
+
+                            value={firstName}
+                            onChange={this.handleChange}
+                        />
+                    </Grid>
+                    <Grid item={true} xs={12}>
+                        <TextField
+                            name="lastName"
+                            label="Last Name"
+                            margin="dense"
+
+                            value={lastName}
+                            onChange={this.handleChange}
+                        />
+                    </Grid>
+
+                    <Grid item={true} xs={12}>
+                        <TextField
+                            name="email"
+                            label="Email"
+                            margin="dense"
+
+                            value={email}
+                            onChange={this.handleChange}
+                        />
+                    </Grid>
+
+                    <Grid item={true} xs={12}>
+                        <TextField
+                            name="username"
+                            label="Username"
+                            margin="dense"
+
+                            value={username}
+                            onChange={this.handleChange}
+                        />
+                    </Grid>
+
+                    <Grid item={true} xs={12}>
+                        <TextField
+                            name="password"
+                            label="Password"
+                            margin="dense"
+                            type="password"
+                            value={password}
+                            onChange={this.handleChange}
+                        />
+                    </Grid>
+
+                    <Grid item={true} xs={12}>
+                        <Button variant='contained' color='primary' type="submit">Submit</Button>
+                    </Grid>
+                </Grid>
+
+
+
             </form>
         )
     }
+
+    private handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    private handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        // tslint:disable-next-line
+        console.log('Submitting form', this.state);
+    };
+
 }
 
 export default Form;
