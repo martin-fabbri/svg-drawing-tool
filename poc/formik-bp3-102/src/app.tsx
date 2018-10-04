d1ce652a3b10800ec7936ebfc2148b7103e8e6f8
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-10-03 19:24:41 -0700

    Formik-bp3 -> Adding select support.

--- poc/formik-bp3-102/src/app.tsx
+++ poc/formik-bp3-102/src/app.tsx
@@ -1,11 +1,14 @@
-import {Button, Classes, FormGroup, InputGroup, Intent, Switch} from "@blueprintjs/core";
+import {Button, Classes, FormGroup, InputGroup, Intent, MenuItem, Switch} from "@blueprintjs/core";
+import {ItemRenderer, Select} from "@blueprintjs/select";
 import {Field, FieldProps, Form, Formik, FormikProps} from "formik";
 import * as React from 'react';
+import {IPlans, OfferedPlans} from "./plans";
 
 interface IShapeValues {
     email: string;
     password: string;
     subscribe: boolean;
+    plan: string;
 }
 
 const validateEmail = (value: string) => {
@@ -57,6 +60,48 @@ const subscribeSwitch = ({field, form}: FieldProps<IShapeValues>) => (
         </FormGroup>
 );
 
+const PlanSelect = Select.ofType<IPlans>();
+
+const planRender: ItemRenderer<IPlans> = (plan, { handleClick, modifiers }) => {
+    if (!modifiers.matchesPredicate) {
+        return null;
+    }
+    return (
+        <MenuItem
+            active={modifiers.active}
+            disabled={modifiers.disabled}
+            key={plan.name}
+            onClick={handleClick}
+            text={plan.name}
+        />
+    );
+};
+
+const validatePlan = (value: string) => {
+    return false;
+};
+
+const plansSelect = ({field, form}: FieldProps<IShapeValues>) => {
+    const handlePlanSelect = (plan: IPlans) => {
+        // console.log('set filed value', plan.name);
+        form.setFieldValue('plan', plan.name);
+    };
+    return (
+        <FormGroup
+            helperText={form.touched.subscribe && form.errors.subscribe ? form.errors.subscribe : ''}>
+            <PlanSelect
+                items={OfferedPlans}
+                itemRenderer={planRender}
+                onItemSelect={handlePlanSelect}
+                filterable={false}
+                {...field}
+            >
+                <Button {...field} rightIcon="double-caret-vertical" text={field.value}/>
+            </PlanSelect>
+        </FormGroup>
+    );
+};
+
 const render = ({isSubmitting}: FormikProps<IShapeValues>) => {
     return (
         <Form>
@@ -75,6 +120,11 @@ const render = ({isSubmitting}: FormikProps<IShapeValues>) => {
                 render={subscribeSwitch}
                 validate={validateSubscribe}
                 type='checkbox'/>
+            <Field
+                name='plan'
+                render={plansSelect}
+                validate={validatePlan}
+                type='select'/>
             <div className={Classes.DIALOG_FOOTER}>
                 <div className={Classes.DIALOG_FOOTER_ACTIONS}>
                     <Button type='button' disabled={isSubmitting}>Cancel</Button>
@@ -86,7 +136,7 @@ const render = ({isSubmitting}: FormikProps<IShapeValues>) => {
 };
 
 const App = () => (
-    <Formik initialValues={{email: '', password: '', subscribe: true}}
+    <Formik initialValues={{email: '', password: '', subscribe: true, plan: 'Free'}}
             onSubmit={submit}
             render={render}/>
 );
---
+++ poc/formik-bp3-102/src/plans.ts
@@ -0,0 +1,9 @@
+export interface IPlans {
+    name: string;
+}
+
+export const OfferedPlans: IPlans[] = [
+    { name: 'Free' },
+    { name: 'Basic' },
+    { name: 'Premium' },
+];
