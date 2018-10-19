635954a25aa9aa8d1225c82f6fe0c265df22ecf8
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-10-19 08:50:50 -0700

    Adding context.

---
+++ src/graph-panel/duck/context.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { GraphPanelTools } from "../constants";
+import { GroupState, PopulationDef } from "../../../reduxStateMgt/types";
+
+export interface IGraphPanelContext {
+  activeTool: GraphPanelTools;
+  onToolSelected: (selectedTool: GraphPanelTools) => void;
+  selectedPopDef?: PopulationDef,
+  selectedGroup?: GroupState,
+}
+
+const GraphPanelContext = React.createContext<IGraphPanelContext>({
+  activeTool: GraphPanelTools.Selection,
+  onToolSelected: (selectedTool: GraphPanelTools) => console.error('onToolSelected has not been implemented'),
+});
+
+export const withGraphPanelContext = <P extends {}>(Component: React.ComponentType<P>) =>
+  class WithContext extends React.PureComponent<P> {
+    public render() {
+      return (
+        <GraphPanelContext.Consumer>
+          {(context: IGraphPanelContext) => <Component {...this.props} {...context} />}
+        </GraphPanelContext.Consumer>
+    );
+    }
+  };
+
+export default GraphPanelContext;
