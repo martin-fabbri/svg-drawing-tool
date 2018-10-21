17043e13fece2bed8ed4b0d65a2486e681008e69
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-10-20 20:54:37 -0700

    Stage Tests.

--- src/features/graph-panel/components/gate-layer/stage.test.tsx
+++ src/features/graph-panel/components/gate-layer/stage.test.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import {GateType, IGateShape} from "../../interfaces";
 import Stage from './stage';
 
-
 const gates: IGateShape[] = [
     {
       dx: 0,
@@ -21,11 +20,34 @@ const gates: IGateShape[] = [
       width: 100,
       x: 100,
       y: 50,
+    },
+    {
+        dx: 12,
+        dy: 24,
+        height: 35,
+        label: {
+            annotation: 'This is a test annotation.',
+            offsetX: 10,
+            offsetY: 10,
+            text: 'Test Gate 1',
+        },
+        name: 'Test Gate 2',
+        type: GateType.Rectangle,
+        uuid: '123456',
+        width: 200,
+        x: 12,
+        y: 27,
     }
 ];
 
-it('renders content', () => {
-  const stage = shallow(<Stage gates={gates}/>);
-  console.log(stage.debug());
-  expect(stage.find('Gate').length).toBeGreaterThanOrEqual(1);
+describe('stage gates', () => {
+    it('renders active gates', () => {
+        const stage = shallow(<Stage gates={gates}/>);
+        expect(stage.find('Gate').length).toEqual(2);
+    });
+
+    it('renders empty gates list', () => {
+        const stage = shallow(<Stage gates={[]}/>);
+        expect(stage.find('Gate').length).toEqual(0);
+    });
 });
